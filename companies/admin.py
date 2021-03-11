24722b212efa882b9dbaeeaa1b5ee64ded4b76bc
Michael Ajanaku <remiljw@gmail.com>
2021-03-11 23:43:24 +0100

    Unregistered Group

--- companies/admin.py
+++ companies/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import User, Company, FavoriteList
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Company)
+admin.site.register(FavoriteList)
+
+admin.site.unregister(Group)
