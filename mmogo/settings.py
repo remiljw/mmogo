13e07980decf6b78e12f13411b9d6dbd24728a8f
Michael Ajanaku <remiljw@gmail.com>
2021-03-12 17:58:20 +0100

    update settings

--- mmogo/settings.py
+++ mmogo/settings.py
@@ -93,9 +93,9 @@ DATABASES = {
 #         'HOST': 'localhost',
 #         'PORT': '5432',
 #     },
-# }
+}
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = db_from_env
 
 
 
