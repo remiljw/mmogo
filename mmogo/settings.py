bdad168239e0c152c50f82003e6f35ed9670e4ac
Michael Ajanaku <remiljw@gmail.com>
2021-03-15 07:06:29 +0100

    settings

--- mmogo/settings.py
+++ mmogo/settings.py
@@ -39,6 +39,8 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://mmogo-frontend.vercel.app",
+    "https://mmogo-webapp.netlify.app",
 ]
 
 
