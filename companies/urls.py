fae6ba1db658f2f786338c0a9d24f30629c98b43
Michael Ajanaku <remiljw@gmail.com>
2021-03-15 17:32:17 +0100

    url

--- companies/urls.py
+++ companies/urls.py
@@ -7,7 +7,7 @@ app_name = 'companies'
 urlpatterns = [
     path('companies/', views.CompanyView.as_view(), name='companies'),
     path('favorites/', views.FavoriteView.as_view(), name='favorite'),
-    path('fav/<int:id>', views.FavoriteDetailView.as_view(), name='detail'),
+    path('fav/<int:id>/', views.FavoriteDetailView.as_view(), name='detail'),
     # path('add/<int:id>', views.AddFavorite.as_view(), name = 'add')
  
 ]
