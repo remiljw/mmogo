5212a118e7c572c64e24020b33e4515c9dc6366e
Michael Ajanaku <remiljw@gmail.com>
2021-03-29 21:18:14 +0100

    security breach

--- mmogo/settings.py
+++ mmogo/settings.py
@@ -156,7 +156,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "remiljw@gmail.com"
-EMAIL_HOST_PASSWORD = "ikdulckwsnzmgcsk"
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 
 
